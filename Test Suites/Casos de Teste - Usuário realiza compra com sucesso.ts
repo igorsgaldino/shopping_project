51bab985fb7e294a8d321239473fcf5e0409cdfa
Igor Galdino <89166943+igorsgaldino@users.noreply.github.com>
2023-07-16 19:02:33 -0300

    Add files via upload

---
+++ Test Suites/Casos de Teste - Usuário realiza compra com sucesso.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Casos de Teste - Usuário realiza compra com sucesso</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>d76cb9be-a79e-4276-bd4a-11099652f5e6</testSuiteGuid>
+   <testCaseLink>
+      <guid>c865988e-93bd-48c0-9737-f88b14c758df</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Casos de Teste - Usuário realiza compra com sucesso</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
