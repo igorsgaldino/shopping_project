07e9c59c19bd4d111318d894f5cbc17357f14100
igorsgaldino <igors@DESKTOP-QRGDN8H.(none)>
2024-04-03 19:57:42 -0300

    Alterando o comando contains

--- cypress/e2e/compra_produtos.cy.js
+++ cypress/e2e/compra_produtos.cy.js
@@ -9,7 +9,7 @@ describe('Compra de Produtos', () => {
     cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click();
     cy.get('[data-test="add-to-cart-sauce-labs-fleece-jacket"]').click();
     cy.get('[data-test="shopping-cart-link"]').click();
-    cy.get('[data-test="shopping-cart-badge"]').should('be.visible').and('contain', '2');  //Verifica quantidade itens
+    cy.get('[data-test="shopping-cart-badge"]').should('be.visible').and('have.text', '2');  //Verifica quantidade itens
     //Usuário clica no botão "Checkout" para prosseguir com a compra
     cy.get('[data-test="checkout"]').click();
     //Usuário preenche os campos para prosseguir com a compra
@@ -19,22 +19,22 @@ describe('Compra de Produtos', () => {
     //Usuário clica no botão "Continue"
     cy.get('[data-test="continue"]').click();
     //Verifica Informação do Pagamento
-    cy.get('[data-test="payment-info-label"]').should('be.visible').and('contain', 'Payment Information:');
-    cy.get('[data-test="payment-info-value"]').should('be.visible').and('contain', 'SauceCard #31337');
+    cy.get('[data-test="payment-info-label"]').should('be.visible').and('have.text', 'Payment Information:');
+    cy.get('[data-test="payment-info-value"]').should('be.visible').and('have.text', 'SauceCard #31337');
     //Verifica Informação de Envio
-    cy.get('[data-test="shipping-info-label"]').should('be.visible').and('contain', 'Shipping Information:');
-    cy.get('[data-test="shipping-info-value"]').should('be.visible').and('contain', 'Free Pony Express Delivery!');
+    cy.get('[data-test="shipping-info-label"]').should('be.visible').and('have.text', 'Shipping Information:');
+    cy.get('[data-test="shipping-info-value"]').should('be.visible').and('have.text', 'Free Pony Express Delivery!');
     //Verifica o Valor Total e Taxa
-    cy.get('[data-test="total-info-label"]').should('be.visible').and('contain', 'Price Total');
-    cy.get('[data-test="subtotal-label"]').should('be.visible').and('contain', 'Item total: $79.98');
-    cy.get('[data-test="tax-label"]').should('be.visible').and('contain', 'Tax: $6.40');
-    cy.get('[data-test="total-label"]').should('be.visible').and('contain', 'Total: $86.38');
+    cy.get('[data-test="total-info-label"]').should('be.visible').and('have.text', 'Price Total');
+    cy.get('[data-test="subtotal-label"]').should('be.visible').and('have.text', 'Item total: $79.98');
+    cy.get('[data-test="tax-label"]').should('be.visible').and('have.text', 'Tax: $6.40');
+    cy.get('[data-test="total-label"]').should('be.visible').and('have.text', 'Total: $86.38');
     //Usuário finaliza compra
     cy.get('[data-test="finish"]').click();
     //Verifica mensagens de compra realizada com sucesso
     cy.get('[data-test="pony-express"]').should('be.visible');
-    cy.get('[data-test="complete-header"]').should('be.visible').and('contain', 'Thank you for your order!');
-    cy.get('[data-test="complete-text"]').should('be.visible').and('contain', 'Your order has been dispatched, and will arrive just as fast as the pony can get there!');
+    cy.get('[data-test="complete-header"]').should('be.visible').and('have.text', 'Thank you for your order!');
+    cy.get('[data-test="complete-text"]').should('be.visible').and('have.text', 'Your order has been dispatched, and will arrive just as fast as the pony can get there!');
     //Usuário clica no botão para retornar para a "Home"
     cy.get('[data-test="back-to-products"]').click();
     //Usuário realiza logout do sistema
--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,26 +2,26 @@ describe('Login', () => {
     it('Caso 1 - Usuário tenta acessar Home sem preencher os dados de Login', () => {
       cy.visit('https://www.saucedemo.com/');
       cy.get('[data-test="login-button"]').click();
-      cy.get('[data-test="error"]').should('be.visible').and('contain', 'Epic sadface: Username is required')
+      cy.get('[data-test="error"]').should('be.visible').and('have.text', 'Epic sadface: Username is required')
     })
     it('Caso 2 - Usuário tenta acessar Home preenchendo apenas o campo Username', () => {
       cy.visit('https://www.saucedemo.com/');
       cy.get('[data-test="username"]').type('nometeste');
       cy.get('[data-test="login-button"]').click();
-      cy.get('[data-test="error"]').should('be.visible').and('contain', 'Epic sadface: Password is required');
+      cy.get('[data-test="error"]').should('be.visible').and('have.text', 'Epic sadface: Password is required');
     })
     it('Caso 3 - Usuário tenta acessar Home preenchendo apenas o campo Password', () => {
       cy.visit('https://www.saucedemo.com/');
       cy.get('[data-test="password"]').type('senhadeteste');
       cy.get('[data-test="login-button"]').click();
-      cy.get('[data-test="error"]').should('be.visible').and('contain', 'Epic sadface: Username is required');
+      cy.get('[data-test="error"]').should('be.visible').and('have.text', 'Epic sadface: Username is required');
     })
     it('Caso 4 - Usuário tenta realizar login com dados inválidos', () => {
       cy.visit('https://www.saucedemo.com/');
       cy.get('[data-test="username"]').type('nometeste');
       cy.get('[data-test="password"]').type('senhadeteste');
       cy.get('[data-test="login-button"]').click();
-      cy.get('[data-test="error"]').should('be.visible').and('contain', 'Epic sadface: Username and password do not match any user in this service');
+      cy.get('[data-test="error"]').should('be.visible').and('have.text', 'Epic sadface: Username and password do not match any user in this service');
     })
     it('Caso 5 - Usuário realiza login com sucesso', () => {
       cy.visit('https://www.saucedemo.com/');
